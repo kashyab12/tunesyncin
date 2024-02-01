02619e170945bbb02f6902e626b1756112dfe7df
kashyab12 <iamkashyab@gmail.com>
2024-02-01 02:31:43 -0800

    bye bye typescript (for now), I will be back maybe

--- server.ts
+++
@@ -1,6 +0,0 @@
-let http: any = require("http");
-
-http.createServer(function (req: any, res: any) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
-}).listen(8080)
